f4b369c69902767ef9086195ac75fa4ffc56c98b
santospanda111 <santospanda111@gmail.com>
2021-08-25 14:12:27 +0530

    added urls for email verification

--- Ideation/UserAuth/urls.py
+++ Ideation/UserAuth/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     path('',views.Index.as_view(),name='homepage'),
     path('/register', views.Register.as_view(), name='user_registration'),
     path('register/<int:pk>/', views.Register.as_view(), name='user_registration'),
-    path('/login', views.LogIn.as_view(), name='user_login')
+    path('/login', views.LogIn.as_view(), name='user_login'),
+    path('verify/<token>',views.VerifyEmail.as_view(),name='verify'),
+    path('verify',views.VerifyEmail.as_view(),name='verify')
 ]
