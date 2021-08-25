67dd44f255d5faf86c7faa12f9e588a9219ea3fb
santospanda111 <santospanda111@gmail.com>
2021-08-25 13:58:05 +0530

    Added LoginAPI views and urls

--- Ideation/UserAuth/urls.py
+++ Ideation/UserAuth/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('',views.Index.as_view(),name='homepage'),
     path('/register', views.Register.as_view(), name='user_registration'),
     path('register/<int:pk>/', views.Register.as_view(), name='user_registration'),
+    path('/login', views.LogIn.as_view(), name='user_login')
 ]
