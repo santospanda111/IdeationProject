e1b88e4080cc7e962cc4917a7da614305dbb397a
santospanda111 <santospanda111@gmail.com>
2021-08-25 13:20:57 +0530

    Added registerApi with models and serializer

--- Ideation/UserAuth/urls.py
+++ Ideation/UserAuth/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from UserAuth import views
 
 urlpatterns=[
-    path('',views.Index.as_view(),name='homepage')
+    path('',views.Index.as_view(),name='homepage'),
+    path('/register', views.Register.as_view(), name='user_registration'),
+    path('register/<int:pk>/', views.Register.as_view(), name='user_registration'),
 ]
